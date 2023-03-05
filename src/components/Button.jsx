76f475792b7df0c215bb2998a25c763e1ce9dfcc
Manthan Kuber <manthankuber873@gmail.com>
2023-03-05 19:27:19 +0530

    refactor(button): add large version prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import styles from "../styles/Button.module.css";
 
-const Button = ({ bgColor, txtColor, children, hasBorder = false }) => {
+const Button = ({ bgColor, txtColor, children, hasBorder = false,isLarge }) => {
   return (
     <button
-      className={`${styles.button} ${hasBorder && styles.button_border} `}
+      className={`${styles.button} ${hasBorder && styles.button_border} ${isLarge && styles.large}`}
       style={{ backgroundColor: bgColor, color: txtColor }}
     >
       {children}

5d5666c06554a6b1d3097a24a63ca037d1b15e9c
Manthan Kuber <manthankuber873@gmail.com>
2023-03-06 09:06:45 +0530

    refactor: make misc changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import MediaButtons from "@/components/MediaButtons";
 const gothic = Gothic_A1({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
+  variable:"--gothic-font"
 });
 
 const zilla = Zilla_Slab({
@@ -25,7 +26,7 @@ const rubik = Rubik({
 
 export default function Home() {
   return (
-    <div className={gothic.className}>
+    <>
       <Head>
         <title>Wetrack Landing Page</title>
         <meta
@@ -35,56 +36,60 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <main className={styles.main}>
-        <div className={styles.left}>
-          <h1>Keep your business organized in just one application</h1>
-          <span>
-            Bill with downloadable statistics,measure profitability,manage team
-            workloads,manage budgets and track performance
-          </span>
-          <div className={styles.btn_trusted_wrapper}>
-            <div className={styles.btn_wrapper}>
-              <Button bgColor="black" txtColor="white" isLarge>
-                Try for free
-              </Button>
-              <Button hasBorder={true} bgColor="#f59fec" isLarge>
-                Learn More
-              </Button>
-            </div>
-            <div className={`${styles.trusted} ${zilla.variable}`}>
-              <span>Trusted by greatest</span>
-              <hr />
-            </div>
-            <IconsList />
-          </div>
-        </div>
-        <div>
-          {/* Project */}
-          <div className={styles.project}>
-            <div className={styles.project_name}>
-              <span>AMAZON PROJECT</span>
-              <span>TUE,30 AUG</span>
-            </div>
-            <span>Architecture Analysis</span>
-            <div className={styles.project_timer_wrapper}>
-              <MediaButtons />
-              <span className={`${styles.timer} ${rubik.variable}`}>1:38:47</span>
+      <div className={gothic.className}>
+        <Header />
+        <main className={styles.main}>
+          <div className={styles.left}>
+            <h1>Keep your business organized in just one application</h1>
+            <span>
+              Bill with downloadable statistics,measure profitability,manage
+              team workloads,manage budgets and track performance
+            </span>
+            <div className={styles.btn_trusted_wrapper}>
+              <div className={`${styles.btn_wrapper} ${gothic.variable}`}>
+                <Button bgColor="black" txtColor="white" isLarge>
+                  Try for free
+                </Button>
+                <Button hasBorder={true} bgColor="#f59fec" isLarge>
+                  Learn More
+                </Button>
+              </div>
+              <div className={`${styles.trusted} ${zilla.variable}`}>
+                <span>Trusted by greatest</span>
+                <hr />
+              </div>
+              <IconsList />
             </div>
           </div>
-          {/* Employee  */}
-          <div></div>
-          {/* Double card */}
           <div>
-            {/* Card 1 */}
+            {/* Project */}
+            <div className={styles.project}>
+              <div className={styles.project_name}>
+                <span>AMAZON PROJECT</span>
+                <span>TUE,30 AUG</span>
+              </div>
+              <span>Architecture Analysis</span>
+              <div className={styles.project_timer_wrapper}>
+                <MediaButtons />
+                <span className={`${styles.timer} ${rubik.variable}`}>
+                  1:38:47
+                </span>
+              </div>
+            </div>
+            {/* Employee  */}
             <div></div>
-            {/* Card 2  */}
-            <div></div>
+            {/* Double card */}
+            <div>
+              {/* Card 1 */}
+              <div></div>
+              {/* Card 2  */}
+              <div></div>
+            </div>
           </div>
-        </div>
-        {/* List of smol cards/tags */}
-        <div></div>
-      </main>
-    </div>
+          {/* List of smol cards/tags */}
+          <div></div>
+        </main>
+      </div>
+    </>
   );
 }
